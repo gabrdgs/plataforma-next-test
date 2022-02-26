e5843abecca06bf0b85cb6732d0b922dcd9384a3
Gabriela Rodrigues <57119264+gabrdgs@users.noreply.github.com>
2022-02-26 16:40:37 -0300

    uptade MailChimp

---
+++ src/components/LeadForm/CustomForm.jsx
@@ -0,0 +1,100 @@
+// Enviando...
+// Email Cadastrado com Sucesso !
+// Email Cadastrado com Sucesso !
+import {Fragment} from 'react'
+import { Col, Row, Input, Form, Space } from 'antd';
+import Image from 'next/image';
+
+import styles from './CustomForm.module.scss';
+
+import {ButtonModel} from '../ButtonModel'
+
+
+export function CustomForm({status, message, onValidated}) {
+  let email, name
+
+  const submit = () =>
+    email &&
+    name &&
+    email.value.indexOf('@') > -1 &&
+    onValidated({
+      EMAIL: email.value,
+      NAME: name.value,
+    })
+
+  function createMarkup(message) {
+    return {__html: message}
+  }
+
+  return (
+    <div className="">
+      <div
+        className=""
+        style={{
+          borderRadius: 2,
+          padding: 10,
+        }}
+      >
+        {status !== 'success' && (
+          <Fragment>
+            {status === 'error' && (
+              <div
+                className="alert error-message"
+                role="alert"
+                dangerouslySetInnerHTML={createMarkup(message)}
+              />
+            )}
+            <Col>
+              <Row span={15}>
+                <input 
+                className={styles.CustomForm}
+                style={{
+                  width: 550,
+                  height: 50,
+                  borderRadius:5,
+                  paddingLeft:10,
+                }}
+                ref={(node) => (name = node)}
+                type="text"
+                placeholder="Nome"
+                />
+              </Row>
+              <br/>
+              <Row>
+                  <input
+                  className= {styles.CustomForm}
+                  style={{
+                    width: 550,
+                    height: 50,
+                    borderRadius:5,
+                    paddingLeft:10,
+                    fontSize: '1em',
+                    padding: 10
+                  }}
+                  ref={(node) => (email = node)}
+                  type="email"
+                  placeholder="Digite seu melhor email"
+                  />
+              </Row>
+            </Col>
+          </Fragment>
+        )}
+        <br/>
+        <ButtonModel
+          width="large"
+          height = "large"
+          class={` submitButton ${
+            status === 'success' ? 'submitButton--success' : ''
+          }`}
+          onClick={submit}
+          hoverScale="true"
+        >
+          {status === 'sending' && 'Enviando...'}
+          {status !== 'success' && status !== 'sending'
+            ? 'Inscreva-se'
+            : message}
+        </ButtonModel>
+      </div>
+    </div>
+  )
+}
--- src/views/LandingPage/Footer/Footer.jsx
+++ src/views/LandingPage/Footer/Footer.jsx
@@ -6,13 +6,14 @@ import { Section } from '../../../components/Section';
 import { Heading } from '../../../components/Heading';
 import { ButtonModel } from '../../../components/ButtonModel';
 import { Paragraph } from '../../../components/Paragraph';
+import { LeadForm } from '../../../components/LeadForm'
 import {SocialMedia} from '../../../components/SocialMedia'
 import {Logo} from '../../../components/Logo'
 
 
 const layout = {
-    labelCol: { span: 10 },
-    wrapperCol: { span: 25 },
+    labelCol: { span: 20 },
+    wrapperCol: { span: 10 },
   };
   
   
@@ -32,21 +33,13 @@ export default function Footer ({ id }){
     return(
         <Section bgColor="dark">
             <Col span={10} offset={7}>
-                <Form {...layout} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages} justify="space-around">
-                    <Heading alignment="center" level={3} color="secondary">Gostou da nossa proposta ?</Heading>
-                    <Paragraph alignment="center" color="secondary">Estamos trabalhando para trazer para vocês a melhor e mais amada plataforma de mentoria do Brasil, se você tem interesse em ser jovem ou mentor, assine nossa newsletter e receba todas as novidades e atualizações sobre o funcionamento da nossa plataforma.</Paragraph>
+                <Heading alignment="center" level={3} color="secondary">Gostou da nossa proposta ?</Heading>
+                <Paragraph alignment="center" color="secondary">Estamos trabalhando para trazer para vocês a melhor e mais amada plataforma de mentoria do Brasil, se você tem interesse em ser jovem ou mentor, assine nossa newsletter e receba todas as novidades e atualizações sobre o funcionamento da nossa plataforma.</Paragraph>    
                     <br></br>
-                    <Form.Item name={['user', 'name']}  rules={[{ required: true }]}>
-                        <Input placeholder="Digite seu Nome"/>
-                    </Form.Item>
-                    <Form.Item name={['user', 'email']}  rules={[{ type: 'email' }]}>
-                        <Input placeholder="Digite seu Email"/>
-                    </Form.Item>
-                    <Form.Item>
-                    <ButtonModel color="default" width='full' height='large'>Enviar</ButtonModel>
-                    </Form.Item>
-                </Form>
+                <LeadForm/>
+             
             </Col>
+            <br></br>
             <Col span={15} offset={10}>
                 <SocialMedia/>
             </Col>
